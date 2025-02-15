de893163738ff4ef1fde4dc4e2a3ee40fd0bd463
Sandeep <sandeepbandari27@gmail.com>
2025-02-15 10:23:16 +0530

    Updated calculator project with recent changes

--- src/main/java/com/example/Calculator/C_controller.java
+++ src/main/java/com/example/Calculator/C_controller.java
@@ -1,5 +1,7 @@
 package com.example.Calculator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,29 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/calculator")
 
 public class C_controller 
-{
+{ 
+	private static final Logger logger = LoggerFactory.getLogger(C_controller.class);
 	@Autowired
 	Calculation service;
      @PostMapping("/{operationtype}")
      public ResponseEntity<CalculationEntity> m1(@PathVariable String operationtype,@RequestParam double value1,@RequestParam double value2) throws Exception
      {
+    	 logger.info("Post request received for operation:{},value1:{},value2:{}",operationtype,value1,value2);
     	 CalculationEntity results=service.operation(operationtype,value1,value2);
-    	return ResponseEntity.ok(results);
+         logger.info("Operation result: {}", results);
+          return ResponseEntity.ok(results);
      }
      @GetMapping("/{operationtype}")
      public ResponseEntity <CalculationEntity> m1Get(@PathVariable String operationtype,
                                      @RequestParam double value1,
                                      @RequestParam double value2) throws Exception
      {
+         logger.info("GET request received for operation: {}, value1: {}, value2: {}", operationtype, value1, value2);
     	 CalculationEntity results=service.operation(operationtype, value1, value2);
-         return ResponseEntity.ok(results);
+         logger.info("Operation result: {}", results);
+           return ResponseEntity.ok(results);
          
          
      }
      @ExceptionHandler(InvalidException.class)
      public ResponseEntity<String>handleInvalidException(InvalidException e)
      {
-    	 return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+         logger.error("InvalidException occurred: {}", e.getMessage());
+           return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
      }
       
 }
--- src/main/java/com/example/Calculator/Calculation.java
+++ src/main/java/com/example/Calculator/Calculation.java
@@ -1,37 +1,49 @@
 package com.example.Calculator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
 public class Calculation 
 {
+	private static final Logger logger=LoggerFactory.getLogger(Calculation.class);
        @Autowired
        CalculatorRepository repository;
        
 	public CalculationEntity operation(String operationtype,double value1,double value2) throws Exception
 	{
+		logger.info("Performing operation:{} with value1:{} and value2:{}",operationtype,value1,value2);
     	   double result;
 		if(operationtype.equalsIgnoreCase("add"))
 		{
 			result=value1+value2;
+			logger.debug("Addition result:{}",result);
 		}
 		else if(operationtype.equalsIgnoreCase("sub"))
 		{
 			result=value1-value2;
+			logger.debug("Substraction result:{}",result);
+
 		}
 		else if(operationtype.equalsIgnoreCase("mul"))
 		{
 			result=value1*value2;
+			logger.debug("Multiplication result:{}",result);
+
 		}
 		else if(operationtype.equalsIgnoreCase("div"))
 		{
 			if(value2 !=0)
 			{
 			result=value1/value2;
+			logger.debug("Division result:{}",result);
+
 			}
 			else
 			{
-				throw new InvalidException("invalid value1");
+                logger.error("Division by zero attempted with value1: {}", value1);
+                throw new InvalidException("invalid value1");
 			}
 		}
 		else if(operationtype.equalsIgnoreCase("modulus"))
@@ -43,7 +55,8 @@ public class Calculation
 			throw new InvalidException("Invalid operation");
 		}
 		CalculationEntity ce=new CalculationEntity(value1,value2,operationtype,result);
-		return repository.save(ce);
+        logger.info("Saving result to database: {}", ce);
+          return repository.save(ce);
 		
 	}
 }
